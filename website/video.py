acc09abf3b977aa6d3703dca1b6f653a28531a03
liokiyori <alexandre.caraes@isen-ouest.yncrea.fr>
2023-12-19 14:27:41 +0100

    corréction du bender

--- website/video.py
+++ website/video.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template, session, request
-
+from . import db
+from .models import User, Post
+from flask import Blueprint, redirect, url_for, render_template, request, session
 
 video = Blueprint("video", __name__)
 
 @video.route('/')
 def videol():
-    return render_template('video.html')
+    if 'email' not in session:
+        return redirect(url_for('auth.login'))
+    name = User.query.filter_by(email=session['email']).first().name
+    return render_template('video.html', name = name)

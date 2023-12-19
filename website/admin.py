52b99d9cd93ddbffd5bdb33c4675e2c3b2efa018
Hatchi-Kin <j.needham@hotmail.fr>
2023-12-19 16:28:17 +0100

    kikoo

--- website/admin.py
+++ website/admin.py
@@ -2,11 +2,12 @@
 from . import db
 from .models import User
 from .utils import extract_embedding
+from datetime import datetime
 import os
 import pandas as pd
 from scipy.spatial.distance import cosine
 from keras_facenet import FaceNet
-from flask import Blueprint, render_template, redirect, url_for, send_file, session, current_app
+from flask import Blueprint, render_template, redirect, url_for, send_file, session, current_app, request
 
 
 path_to_mega_faces_dataset = "website/static/mega_faces_dataset.csv"
@@ -76,3 +77,42 @@ def download_predictions():
     csv_file = os.path.join(current_app.root_path, 'static', 'downloaded_predictions.csv')
     df_predicted.to_csv(csv_file, index=False)
     return send_file(csv_file, as_attachment=True)
+
+
+########################################################################
+
+import os
+
+@admin.route('/create_dataset', methods=['GET', 'POST'])
+def create_dataset():
+    if 'email' not in session:
+        return redirect(url_for('auth.login'))
+    user = User.query.filter_by(email=session['email']).first()
+    if not user.is_admin:
+        return redirect(url_for('auth.login'))
+    name = user.name
+    path_to_new_dataset = "website/static/custom_datasets/new_embeddings.csv"
+    df = pd.read_csv(path_to_new_dataset)
+    allowed_celebrities = df[df['target'] == 1]['celebname'].unique()
+    checked_celebrities = []  # Define checked_celebrities here
+    new_df = None
+    if request.method == 'POST':
+        checked_celebrities = [celeb for celeb in allowed_celebrities if request.form.get('checkbox_' + celeb)]
+        # Now checked_celebrities is a list of the values of the checked checkboxes
+        # Create a new df with the rows of the checked celebrities
+        new_df = df[df['celebname'].isin(checked_celebrities)]
+        # Count how many rows in total
+        total_rows = len(new_df)
+        # Pick at random as many rows in the first df where df['target'] == 0
+        df_target_0 = df[df['target'] == 0]
+        random_rows = df_target_0.sample(n=total_rows)
+        # Concatenate the two dfs
+        new_df = pd.concat([new_df, random_rows])
+        # Save the new df to a csv file with the current datetime as the name
+        current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
+        new_df.to_csv(f'website/static/custom_datasets/{current_datetime}_dataset.csv', index=False)
+    
+    # Get a list of all filenames in the custom_datasets directory
+    filenames = os.listdir('website/static/custom_datasets/')
+    
+    return render_template("create_dataset.html", name=name, allowed_celebrities=allowed_celebrities, checked_celebrities=checked_celebrities, filenames=filenames)
